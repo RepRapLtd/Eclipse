aa0cf38f544c379e1ed4784b8595ccc01e343e94
Adrian Bowyer <adrian@reprappro.com>
2019-07-29 22:42:38 +0100

    Small bug fixed...

--- workspace/Electric3DPrinter/pde.cpp
+++ workspace/Electric3DPrinter/pde.cpp
@@ -21,24 +21,24 @@ int main()
 
 	// Boundary conditions
 
-	for(i = 1;i <= n; i++)
+	for(i = 0;i <= n; i++)
 	{
 		a[i][1] = a[i][m] = 0.0;
 	}
-	for(j = 1; j <= m; j++)
+	for(j = 0; j <= m; j++)
 	{
 		a[1][j] = a[n][j] = 0.0;
 	}
 
 	// One source and one sink at the edges
 
-	a[1][m/2] = 10.0;
+	a[0][m/2] = 10.0;
 	a[n][m/2] = -10.0;
 
 	// Initialise the rest to 0
 
-	for(i=2;i<n;i++)
-		for(j=2;j<m;j++)
+	for(i=1;i<n;i++)
+		for(j=1;j<m;j++)
 		{
 			a[i][j] = 0.0;
 			b[i][j] = 0.0;
@@ -49,20 +49,20 @@ int main()
 	double r = 0.0;
 	double rms = 100.0*CONVERGENCE;
 	k = 0;
-	while(k < 1000 && rms > CONVERGENCE)
+	while(k < 2000 && rms > CONVERGENCE)
 	{
 		rms = 0.0;
-		for(i=2;i<n;i++)
+		for(i = 1; i < n; i++)
 		{
-			for(j=2;j<m;j++)
+			for(j = 1; j < m; j++)
 			{
-				a[i][j] = (a[i-1][j]+a[i+1][j]+a[i][j-1]+a[i][j+1])/4;
+				a[i][j] = (a[i-1][j] + a[i+1][j] + a[i][j-1] + a[i][j+1])/4;
 				r = (a[i][j] - b[i][j]);
 				rms = rms + r*r;
 				b[i][j] = a[i][j];
 			}
 		}
-		rms = sqrt(rms/( (float)n*(float)n) );
+		rms = sqrt(rms/( (float)m*(float)n) );
 		printf("Iteration: %i, rms: %f\n", k, rms);
 		k++;
 	}
@@ -70,9 +70,9 @@ int main()
 	// Compute gradient magnitudes in b
 
 	double xd, yd;
-	for(i = 2; i < n; i++)
+	for(i = 1; i < n; i++)
 	{
-		for(j = 2; j < m; j++)
+		for(j = 1; j < m; j++)
 		{
 			xd = 0.5*(a[i+1][j] - a[i-1][j]);
 			yd = 0.5*(a[i][j+1] - a[i][j-1]);
@@ -84,18 +84,20 @@ int main()
 
 	FILE *fp;
 	fp=fopen("potential.dat","w");
-	for(i = 1;i <= n; i++)
+	for(i = 0;i <= n; i++)
 	{
-		for(j = 1; j<=m ; j++)
+		for(j = 0; j <= m ; j++)
 			fprintf(fp,"%f\n",a[i][j]);
 		fprintf(fp,"\n");
 	}
 	fclose(fp);
 
+	// NB the field grid doesn't include the boundaries and so is 2 smaller in each direction.
+
 	fp=fopen("field.dat","w");
-	for(i = 2; i < n; i++)
+	for(i = 1; i < n; i++)
 	{
-		for(j = 2; j < m; j++)
+		for(j = 1; j < m; j++)
 			fprintf(fp,"%f\n",b[i][j]);
 		fprintf(fp,"\n");
 	}
