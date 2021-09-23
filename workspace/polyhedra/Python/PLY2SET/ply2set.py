4c3988fa989da67e160b5e3d65fd513fde460fbe
Adrian Bowyer <adrian@reprappro.com>
2021-09-23 12:42:54 +0100

    Put OpenSCAD output in main program and deleted separate translation program.

--- workspace/polyhedra/Python/PLY2SET/ply2set.py
+++ workspace/polyhedra/Python/PLY2SET/ply2set.py
@@ -67,6 +67,7 @@ debug = False
 # The bounding box and a point near the middle
 positiveCorner = [-sys.float_info.max, -sys.float_info.max, -sys.float_info.max]
 negativeCorner = [sys.float_info.max, sys.float_info.max, sys.float_info.max]
+diagonal = None
 middle = [0, 0, 0]
 
 # We want there to be just one global instance of these
@@ -118,7 +119,7 @@ class TriangleFileData:
 class Triangle:
     def __init__(self, vertices, colour, ply):
         self.vertices = vertices
-        self.neighbours = [-1, -1, -1]
+        self.neighbours = [-1, -1, -1] # Set up later
         self.ply = ply
         self.points = self.Points()
         self.normal = np.cross(np.subtract(self.points[1], self.points[0]), np.subtract(self.points[2], self.points[0]))
@@ -279,6 +280,50 @@ class HalfSpace:
         result = result.replace("+ -", "- ")  # Sigh!
         return result
 
+    # This takes a halfspace and makes an OpenSCAD module() for it.
+    # OpenSCAD doesn't deal in halfSpaces, so we use a giant cube with one of
+    # its faces in the right place.  The module name is sNNN where NNN is the
+    # index of the half space used in the set-theoretic expression that describes the object.
+    def ToOpenSCAD(self, hsIndex):
+        hsString = "module s"+str(round(hsIndex)) + "()\n{\n"
+
+    # The final translation. At this stage we have our cube with its relevant face going through
+    # the origin and the normal of that face pointing in the right direction. All we have to do
+    # is translate it along that normal vector by it's distance from the origin.
+        translate = np.multiply(self.normal, -self.d)
+        translate = " translate([" + str(translate[0]) + ", " + str(translate[1]) + ", " + str(translate[2]) +"])\n"
+        hsString += translate;
+
+    # Our cube will have its top face in the XY plane, with the normal of that face
+    # pointing up the Z axis. The outer product of that normal and the halfspace's
+    # normal will be the axis vector we need to rotate the cube around, and the magnitude
+    # of that vector will be the sine of the angle we need to rotate by.
+        cross = np.cross(self.normal, [0, 0, 1])
+        sine = maths.sqrt(np.dot(cross, cross))
+    # Stop small rounding errors messing with the asin() function.
+        if sine > 1:
+            sine = 1
+        if sine < -1:
+            sine = -1
+
+    # Create the cube and shift it down so it's top face is in the XY plane.
+    # Test if we actually need to rotate it or not.
+        d = -5.0*diagonal
+        if abs(sine) < small:
+            cosine = np.dot(self.normal, [0, 0, 1])
+            if cosine > 0:
+                rotateAndTranslate = " translate([" + str(d) + ", " + str(d) + ", " + str(2.0*d) +"])\n"
+            else:
+                rotateAndTranslate = " translate([" + str(d) + ", " + str(d) + ", 0.0])\n"
+        else:
+            sine = maths.asin(sine)
+            rotateAndTranslate = " rotate(a = " + str(-180.0*sine/maths.pi) + ", v = [" + str(cross[0]) + ", " + str(cross[1]) + ", " + str(cross[2]) + "])\n"
+            rotateAndTranslate += " translate([" + str(d) + ", " + str(d) + ", " + str(2.0*d) +"])\n"
+
+    # Finally make the cube, which all of the above is applied to.
+        d = 10.0*diagonal
+        hsString += rotateAndTranslate + " cube([" + str(d) + ", " + str(d) + ", " + str(d) +"]);\n}\n\n"
+        return hsString
 
 # ********************************************************************************************************
 
@@ -332,6 +377,28 @@ class HalfSpaceList:
 
 
 # ********************************************************************************************************
+
+# Take a set theoretic expression like 7 & 12 (where 7 and 12 are half-space
+# indices) and create the OpenSCAD expression that does that with module s7()
+# and so on.
+def SetExpression(o, a, b):
+ if o == '|':
+  result = "union()"
+ elif o == '&':
+  result = "intersection()"
+ else:
+  result = "difference()"
+ result += "{"
+ if a[0].isdigit():
+  result += "s" + str(a) + "();"
+ else:
+  result += a + ";"
+ if b[0].isdigit():
+  result += "s" + str(b) + "();"
+ else:
+  result += b + ";"
+ result += "}"
+ return result
 
 # Unions and intersections of half-spaces.
 # Set() gives the empty set
@@ -497,15 +564,28 @@ class Set:
         return rpExpression
 
 
+
+    # Parse the reverse polish set-theoretic expression creating its OpenSCAD equivalent.
+    def ToOpenSCAD(self):
+        rpExpression = self.ToRP()
+        stack=[]
+        for s in rpExpression:
+            if not s == '':
+                if s[0].isdigit():
+                    stack.append(s)
+                else:
+                    stack.append(SetExpression(s, stack.pop(), stack.pop()))
+        return stack[0]
+
 # *******************************************************************************************************
 
 # Graphics
 # ----------
 
-# A model consists of a list of triangles for display. The pattern can be rotated on the screen
+# A scene consists of a list of triangles for display. The pattern can be rotated on the screen
 # and zoomed using the mouse so it can be easily seen.
 
-class Model:
+class Scene:
     def __init__(self, triangles):
         self.triangles = triangles
         self.xangle = 0
@@ -616,7 +696,7 @@ def MakeTriangleWindow(triangles, title):
     # They may be given a different colour after this function is called.
     triangles = copy.deepcopy(triangles)
     world = World()
-    m = Model(triangles)
+    m = Scene(triangles)
     world.AddModel(m)
     PutWorldInWindow(world, title)
 
@@ -834,6 +914,80 @@ def ToFile(fileName, halfSpaces, set):
     halfSpaces.ResetFlags()
 
 
+# Small function to turn things like "[0.0, -0.28, 0.77, -15.9]" from a file
+# and turn them into a Python list. There's probably a library function to do this
+# if I could be bothered to look, but it's quicker to write it myself...
+def GetListFromLine(file):
+ lst = file.readline()
+ lst = lst.replace("[", "")
+ lst = lst.replace("]", "")
+ lst = " ".join( lst.split() )
+ lst = lst.split(" ")
+ result = []
+ for item in lst:
+  result.append(float(item))
+ return result
+'''
+# Read the set file from the ply2set.py program and from it
+# create a list of OpenSCAD halfspace module()s and the OpenSCAD
+# set theory to tie them together.
+def ReadSetFile(fileName):
+ global negativeCorner, positiveCorner, diagonal, halfSpaceList, set
+ file = open(fileName, "r")
+ negativeCorner = GetListFromLine(file)
+ positiveCorner = GetListFromLine(file)
+ diagonal = np.subtract(positiveCorner, negativeCorner)
+ diagonal = maths.sqrt(np.dot(diagonal, diagonal))
+ halfSpaceCount = round(GetListFromLine(file)[0])
+ halfSpaceList = []
+ for hsIndex in range(halfSpaceCount):
+  hs = GetListFromLine(file)
+  normal = [hs[1], hs[2], hs[3]]
+  d = hs[4]
+  hs = (hs[0], [normal, d])
+  halfSpaceList.append(hs)
+ set = file.readline()
+ set = set.replace("\n", "")
+ set = set.split(" ")
+ newList = []
+ for hsIndex in range(len(halfSpaceList)):
+  newList.append(OpenSCADHalfSpace(halfSpaceList[hsIndex]))
+ halfSpaceList = newList
+ ParseSet()
+'''
+
+# Create the .set file
+
+def ToOpenSCAD(fileName, halfSpaces, set):
+    file = open(fileName, "w")
+
+    # Get the RP expression to write out at the end
+    rp = set.ToRP()
+
+    # Count the number of unique halfSpaces in the RP expression and set their flags
+    count = 0
+    for item in rp:
+        if item[0].isdigit():
+            hs = int(item)
+            halfSpace = halfSpaces.Get(hs)
+            if not halfSpace.flag:
+                halfSpace.SetFlag(True)
+                count += 1
+
+    for c in range(len(halfSpaces)):
+        hs = halfSpaces.Get(c)
+        if hs.flag:
+            file.write(hs.ToOpenSCAD(c))
+
+
+    file.write(set.ToOpenSCAD())
+    file.write("\n")
+
+    # Tidy up
+    file.close()
+    halfSpaces.ResetFlags()
+
+
 # ***********************************************************************************************************
 
 # Run the conversion
@@ -841,8 +995,8 @@ def ToFile(fileName, halfSpaces, set):
 
 halfSpaces = HalfSpaceList()
 
-#model = "STL2CSG-test-objects-woo-2"
-model = "projections-bottom-end"
+model = "STL2CSG-test-objects-woo-2"
+#model = "projections-bottom-end"
 place = "../../"
 # fileName = '../../cube.ply'
 # fileName = '../../two-disjoint-cubes.ply'
@@ -867,6 +1021,8 @@ for v in range(triangleFileData.VertexCount()):
     positiveCorner = np.maximum(positiveCorner, vertex)
     negativeCorner = np.minimum(negativeCorner, vertex)
 middle = np.multiply(middle, 1.0 / triangleFileData.VertexCount())
+diagonal = np.subtract(positiveCorner, negativeCorner)
+diagonal = maths.sqrt(np.dot(diagonal, diagonal))
 
 # Make a list of all the original triangles
 originalTriangles = []
@@ -879,7 +1035,7 @@ ConstructNeighbourLists(originalTriangles)
 # The recursive alternating sum of volumes function needs
 # the list of triangles to work with, the .ply file data
 # to get vertex coordinates from, and the current level
-# or recursion (here at the start, 0).
+# of recursion (here at the start, 0).
 trianglesAndPly = (originalTriangles, triangleFileData, 0)
 
 # Run the algorithm
@@ -897,6 +1053,6 @@ points = [
 for point in points:
     print(str(point) + " has value " + str(finalSet.Value(point)))
 '''
-ToFile(model + ".set", halfSpaces, finalSet)
+ToOpenSCAD(model + ".scad", halfSpaces, finalSet)
 if graphics > 0:
     pyglet.app.run()
